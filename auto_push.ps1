36a2856b027253caa24cb5c46baf6675eb4316d4
elizbaryananush <elizbaryananush27@gmail.com>
2025-09-30 22:45:28 +0400

    Auto-commit at 2025-09-30 22:45:28

---
+++ auto_push.ps1
@@ -0,0 +1,12 @@
+# Navigate to your project folder
+Set-Location "C:\Users\user\Desktop\nushik\project1>"
+
+# Add all changes
+git add .
+
+# Commit with timestamp
+$time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+git commit -m "Auto-commit at $time"
+
+# Push to version001 branch
+git push origin version001
