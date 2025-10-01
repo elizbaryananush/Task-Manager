8a9e45c368d765a358fc2c38076dbca393c080ef
elizbaryananush <elizbaryananush27@gmail.com>
2025-10-01 13:37:54 +0400

    initialcommit

--- back_end/src/app.module.ts
+++ back_end/src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'myuser',
+      password: 'mypassword',
+      database: 'myapp',
+      autoLoadEntities: true,   // loads entities automatically
+      synchronize: true,        // auto create tables (turn off in production)
+    }),
+    UsersModule,
+    
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- back_end/src/users/users.controller.ts
+++ back_end/src/users/users.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(
+    @InjectRepository(User)
+    private userRepo: Repository<User>,
+  ) {}
+
+  @Get()
+  findAll() {
+    return this.userRepo.find();
+  }
+
+  @Post()
+  create(@Body() userData: Partial<User>) {
+    const user = this.userRepo.create(userData);
+    return this.userRepo.save(user);
+  }
+}
